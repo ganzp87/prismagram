89a3f8d8d09b5a51625a0f2d9bc970a3f3637666
Ganzp <ganzp@naver.com>
2020-03-30 22:39:54 +0900

    add followcount

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -24,8 +24,20 @@ export default {
 		},
 		following: ({ id }) => prisma.user({ id }).following(),
 		followers: ({ id }) => prisma.user({ id }).followers(),
+		followingCount: async ({ id }) => {
+			const following = await prisma.user({ id }).following()
+			return following.length
+		},
+		followersCount: async ({ id }) => {
+			const followers = await prisma.user({ id }).followers()
+			return followers.length
+		},
 		likes: ({ id }) => prisma.user({ id }).likes(),
 		posts: ({ id }) => prisma.user({ id }).posts(),
+		postsCount: async ({ id }) => {
+			const posts = await prisma.user({ id }).posts()
+			return posts.length
+		},
 		comments: ({ id }) => prisma.user({ id }).comments(),
 		rooms: ({ id }) => prisma.user({ id }).rooms()
 	}
